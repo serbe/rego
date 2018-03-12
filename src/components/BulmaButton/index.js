1e2d0e965b413c9757ded12d90ed0612fb840dcf
Valeriy Kostikov <mkoctuk@gmail.com>
2018-03-13 00:02:31 +0300

    add props

--- src/components/BulmaButton/index.js
+++ src/components/BulmaButton/index.js
@@ -1,58 +1,73 @@
 import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 // import { NavLink } from 'react-router-dom';
 
 class BulmaButton extends Component {
-  // static contextTypes = {
-  //   router: PropTypes.object
-  // }
-  constructor(props) {
-    super(props);
-    // this.state = { hasError: false };
+  static defaultProps = {
+    text: "",
+    color: "",
+    size: "",
+    outlined: false,
+    inverted: false,
+    rounded: false,
+    hovered: false,
+    focused: false,
+    active: false,
+    loading: false,
+    static: false,
+    disabled: false,
   }
 
-  // constructor(props, context) {
-  //   super(props, context);
-  //   this.state = {
-  //     logged: true,
-  //     open: false,
-  //     auth: true,
-  //   };
-  // }
+  static propTypes = {
+    text: PropTypes.string,
+    color: PropTypes.string,
+    size: PropTypes.string,
+    outlined: PropTypes.bool,
+    inverted: PropTypes.bool,
+    rounded: PropTypes.bool,
+    hovered: PropTypes.bool,
+    focused: PropTypes.bool,
+    active: PropTypes.bool,
+    loading: PropTypes.bool,
+    static: PropTypes.bool,
+    disabled: PropTypes.bool,
+  }
 
   render() {
     let buttonClass = "button";
-    if (this.props.color) {
+    if (!!this.props.color) {
       buttonClass = buttonClass + ` is-${this.props.color}`;
     }
-    if (this.props.size) {
+    if (!!this.props.size) {
       buttonClass = buttonClass + ` is-${this.props.size}`;
     }
-    if (this.props.state) {
+    if (!!this.props.state) {
       buttonClass = buttonClass + ` is-${this.props.state}`;
     }
 
-    let IconButton = null;
-    if (this.props.icon) {
-      IconButton = <bulma-icon size="this.props.size" icon="this.props.icon" position="this.props.iconPosition" color="this.props.color"/>
-    }
+    let IconButton = <React.Fragment></React.Fragment>;
+    // if (!!this.props.icon) {
+    //   IconButton = <bulma-icon size={this.props.size} icon={this.props.icon} position={this.props.iconPosition} color={this.props.color}/>
+    // }
+
+    let ButtonText = <React.Fragment></React.Fragment>
+    // if (!!this.props.text) {
+    //   ButtonText = <React.Fragment>{this.props.text}</React.Fragment>
+    // }
+
+                // <IconButton/>
+            // <ButtonText/>
 
     return (
       <div className="field">
         <p className="control">
-          <a className={buttonClass} onClick="click">
-            <IconButton/>
-            { this.props.text }
+          <a className={buttonClass} disabled={this.props.disabled}>
+            <ButtonText/>
           </a>
         </p>
       </div>
     );
   }
 }
-
-BulmaButton.propTypes = {
-  // color: PropTypes.string,
-
-};
 
 export default BulmaButton;
