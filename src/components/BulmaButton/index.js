5e7a4fcb31217ef0979c7d63aa6aa342c7522db3
Valeriy Kostikov <mkoctuk@gmail.com>
2018-04-15 00:59:26 +0300

    new way

--- src/components/BulmaButton/index.js
+++ src/components/BulmaButton/index.js
@@ -1,29 +1,16 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
+import cc from 'classcat';
 import BulmaIcon from 'components/BulmaIcon';
 
-export default class BulmaButton extends Component {
-	static defaultProps = {
-    classes: '',
-		text: '',
-		color: '',
-		size: '',
-		outlined: false,
-		inverted: false,
-		rounded: false,
-		hovered: false,
-		focused: false,
-		active: false,
-		loading: false,
-		static: false,
-		disabled: false
-	};
-
+export default class BulmaButton extends PureComponent {
 	static propTypes = {
-    classes: PropTypes.string,
-		text: PropTypes.string,
-		color: PropTypes.string,
-		size: PropTypes.string,
+    children: PropTypes.node,
+		className: PropTypes.string,
+		href: PropTypes.string,
+		onClick: PropTypes.func,
+		color: PropTypes.oneOf('white', 'light', 'dark', 'text', 'primary', 'link', 'info', 'success', 'warning', 'danger'),
+		size: PropTypes.oneOf('small', 'medium', 'large'),
 		outlined: PropTypes.bool,
 		inverted: PropTypes.bool,
 		rounded: PropTypes.bool,
@@ -35,67 +22,69 @@ export default class BulmaButton extends Component {
 		disabled: PropTypes.bool
 	};
 
+	static defaultProps = {
+		children: null,
+    className: '',
+		href: '',
+		onClick: () => null,
+		color: null,
+		size: null,
+		outlined: false,
+		inverted: false,
+		rounded: false,
+		hovered: false,
+		focused: false,
+		active: false,
+		loading: false,
+		static: false,
+		disabled: false
+	};
+
 	render() {
-    let buttonClass = 'button';
-    if (!!this.props.classes) {
-      buttonClass = `${this.props.classes} ${buttonClass}`;
-    }
-		if (!!this.props.color) {
-			buttonClass = `${buttonClass} is-${this.props.color}`;
-		}
-		if (!!this.props.size) {
-			buttonClass = `${buttonClass} is-${this.props.size}`;
-		}
-		if (this.props.outlined) {
-			buttonClass = `${buttonClass} is-outlined`;
-		}
-		if (this.props.inverted) {
-			buttonClass = `${buttonClass} is-inverted`;
-    }
-    if (this.props.rounded) {
-			buttonClass = `${buttonClass} is-rounded`;
-    }
-    if (this.props.hovered) {
-			buttonClass = `${buttonClass} is-hovered`;
-    }
-    if (this.props.focused) {
-			buttonClass = `${buttonClass} is-focused`;
-    }
-    if (this.props.active) {
-			buttonClass = `${buttonClass} is-active`;
-    }
-    if (this.props.loading) {
-			buttonClass = `${buttonClass} is-loading`;
-    }
-    if (this.props.static) {
-			buttonClass = `${buttonClass} is-static`;
-    }
-    if (this.props.rounded) {
-			buttonClass = `${buttonClass} is-rounded`;
-		}
-
-		let IconButton;
-		if (!!this.props.icon) {
-			IconButton = (
-				<BulmaIcon
-					size={this.props.size}
-					icon={this.props.icon}
-					position={this.props.iconPosition}
-					color={this.props.color}
-				/>
-			);
-		}
-
-		let ButtonText;
-		if (!!this.props.text) {
-			ButtonText = <React.Fragment>{this.props.text}</React.Fragment>;
-		}
+		const {
+			children,
+			className,
+			href,
+			onClick,
+			color,
+			size,
+			outlined,
+			inverted,
+			rounded,
+			hovered,
+			focused,
+			active,
+			loading,
+			static,
+			disabled,
+			...props
+		} = this.props;
+		let Button = static ? 'span' : 'a';
 
 		return (
-      <a className={buttonClass} disabled={this.props.disabled}>
-        {IconButton}
-        {ButtonText}
-      </a>
+			<Button
+				{...props}
+				disabled={disabled}
+				onClick={disabled ? undefined : onClick}
+				className={cc([
+					{className},
+					'button',
+					{
+						[`is-${color}`]: color,
+						[`is-${size}`]: size,
+						'is-outlined': outlined,
+						'is-inverted': inverted,
+						'is-rounded': rounded,
+						'is-hovered': hovered,
+						'is-focused': focused,
+						'is-active': active,
+						'is-loading': loading,
+						'is-static': static
+					}
+				])}
+			>
+        {children}
+      </Button>
 		);
 	}
 }
