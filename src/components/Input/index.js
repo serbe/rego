f18b5cfea2b73aa357880b877fc99171ecbb0356
Serbe <mkoctuk@gmail.com>
2019-02-12 11:49:11 +0300

    add icon

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,80 +1,114 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import cc from 'classcat';
+import React from "react";
+import PropTypes from "prop-types";
+import cc from "classcat";
+import Icon from "./components/Icon";
 
-export default class componentName extends React.Component {
-  static propTypes = {
-		className: PropTypes.string,
-		// onClick: PropTypes.func,
-		type: PropTypes.oneOf('text', 'password', 'email', 'tel'),
-		value: PropTypes.string,
-		placeholder: PropTypes.string,
-		color: PropTypes.oneOf('primary', 'info', 'success', 'warning', 'danger'),
-		size: PropTypes.oneOf('small', 'medium', 'large'),
-		rounded: PropTypes.bool,
-		hovered: PropTypes.bool,
-		focused: PropTypes.bool,
-		disabled: PropTypes.bool,
-    readonly: PropTypes.bool,
-		isStatic: PropTypes.bool
-  };
+class Input extends React.Component {
+  constructor(props) {
+    super(props);
 
-	static defaultProps = {
-    className: '',
-		// onClick: () => null,
-		type: 'text',
-		value: '',
-		placeholder: null,
-		color: null,
-		size: null,
-		rounded: false,
-		hovered: false,
-		focused: false,
-		disabled: false,
-		readonly: false,
-		isStatic: false
-  };
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick(e) {
+    if (this.props.disabled) {
+      e.preventDefault();
+      return;
+    }
+
+    if (this.props.onClick) {
+      this.props.onClick(e);
+    }
+  }
 
   render() {
-		const {
-			className,
-			// onClick,
-			type,
-			value,
-			placeholder,
-			color,
-			size,
-			rounded,
-			hovered,
-			focused,
-			disabled,
-			readonly,
-			isStatic,
-			...props
-		} = this.props;
+    let {
+      type,
+      color,
+      size,
+      round,
+      hover,
+      focus,
+      loading,
+      disable,
+      readonly,
+      isStatic,
+      iconLeft,
+      iconRight,
+      value,
+      placeholder,
+      className
+    } = this.props;
+
+    let divClasses = cc([
+      "control",
+      {
+        [`is-${size}`]: size,
+        "has-icons-left": iconLeft,
+        "has-icons-right": iconRight,
+        "is-loading": loading
+      }
+    ]);
+    let inputClasses = cc([
+      { className },
+      "input",
+      {
+        [`is-${color}`]: color,
+        [`is-${size}`]: size,
+        "is-rounded": round,
+        "is-hovered": hover,
+        "is-focused": focus,
+        "is-static": isStatic
+      }
+    ]);
+    let LeftIcon = null;
+    if (iconLeft) {
+      LeftIcon = <Icon position={"left"} icon={iconLeft} />;
+    }
+    let RightIcon = null;
+    if (iconRight) {
+      RightIcon = <Icon position={"right"} icon={iconRight} />;
+    }
 
     return (
-			<input
-				{...props}
-				type={type}
-				value={value}
-				placeholder={placeholder}
-				disabled={disabled}
-				readonly={readonly}
-				className={cc([
-					{className},
-					'input',
-					{
-						[`is-${color}`]: color,
-						[`is-${size}`]: size,
-						'is-rounded': rounded,
-						'is-hovered': hovered,
-						'is-focused': focused,
-						'is-active': active,
-						'is-static': isStatic,
-					}
-				])}
-      />
-    )
+      <div className={divClasses}>
+        <input
+          type={type}
+          value={value}
+          placeholder={placeholder}
+          disabled={disable}
+          readonly={readonly}
+          className={inputClasses}
+          onClick={this.onClick}
+        />
+        <LeftIcon />
+        <RightIcon />
+      </div>
+    );
   }
 }
+
+Input.propTypes = {
+  type: PropTypes.oneOf("text", "password", "email", "tel"),
+  color: PropTypes.oneOf("primary", "info", "success", "warning", "danger"),
+  size: PropTypes.oneOf("small", "normal", "medium", "large"),
+  round: PropTypes.bool,
+  hover: PropTypes.bool,
+  focus: PropTypes.bool,
+  loading: PropTypes.bool,
+  disable: PropTypes.bool,
+  readonly: PropTypes.bool,
+  isStatic: PropTypes.bool,
+  iconLeft: PropTypes.string,
+  iconRight: PropTypes.string,
+  value: PropTypes.string,
+  placeholder: PropTypes.string,
+  className: PropTypes.string,
+  onClick: PropTypes.func
+};
+
+Input.defaultProps = {
+  type: "text"
+};
+
+export default Input;
