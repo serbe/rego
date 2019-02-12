46b8f63d38836a8cb36ff964947d51b9a1998904
Serbe <mkoctuk@gmail.com>
2019-02-12 11:48:44 +0300

    add position

--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -4,14 +4,15 @@ import cc from "classcat";
 
 class Icon extends React.Component {
   render() {
-    const { children, className, color, size, icon } = this.props;
+    const { children, className, color, size, position, icon } = this.props;
 
     let spanClasses = cc([
       { className },
       "icon",
       {
         [`has-text-${color}`]: color,
-        [`is-${size}`]: size
+        [`is-${size}`]: size,
+        [`is-${position}`]: position,
       }
     ]);
 
@@ -28,6 +29,7 @@ class Icon extends React.Component {
 Icon.propTypes = {
   color: PropTypes.oneOf("info", "success", "warning", "danger"),
   size: PropTypes.oneOf("small", "normal", "medium", "large"),
+  position: PropTypes.oneOf("left", "right"),
   icon: PropTypes.string.isRequired,
   children: PropTypes.node,
   className: PropTypes.string
