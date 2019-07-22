f9e99541f2d4c6c6815c9b84c1caf8738f8e4ec3
Serbe <mkoctuk@gmail.com>
2019-07-22 11:45:16 +0300

    tweak

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,30 +2,31 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 
-import clsx from 'clsx';
-import { makeStyles } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Drawer from '@material-ui/core/Drawer';
-import Button from '@material-ui/core/Button';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import Badge from '@material-ui/core/Badge';
-import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
-import Link from '@material-ui/core/Link';
-import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import Icon from '@material-ui/core/Icon';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListSubheader from '@material-ui/core/ListSubheader';
-import NotificationsIcon from '@material-ui/icons/Notifications';
+import clsx from "clsx";
+import { makeStyles } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Drawer from "@material-ui/core/Drawer";
+import Button from "@material-ui/core/Button";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import List from "@material-ui/core/List";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import Badge from "@material-ui/core/Badge";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import Link from "@material-ui/core/Link";
+import MenuIcon from "@material-ui/icons/Menu";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import KitchenIcon from "@material-ui/icons/Kitchen";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import Icon from "@material-ui/core/Icon";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListSubheader from "@material-ui/core/ListSubheader";
+import NotificationsIcon from "@material-ui/icons/Notifications";
 
 // import PropTypes from "prop-types";
 
@@ -35,154 +36,216 @@ import NotificationsIcon from '@material-ui/icons/Notifications';
 //   };
 // }
 
+const drawerWidth = 240;
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex"
+  },
+  toolbar: {
+    paddingRight: 24 // keep right padding when drawer closed
+  },
+  toolbarIcon: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    padding: "0 8px",
+    ...theme.mixins.toolbar
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    })
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  },
+  menuButton: {
+    marginRight: 36
+  },
+  menuButtonHidden: {
+    display: "none"
+  },
+  title: {
+    flexGrow: 1
+  },
+  drawerPaper: {
+    position: "relative",
+    whiteSpace: "nowrap",
+    width: drawerWidth,
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  },
+  drawerPaperClose: {
+    overflowX: "hidden",
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    }),
+    width: theme.spacing(7),
+    [theme.breakpoints.up("sm")]: {
+      width: theme.spacing(9)
+    }
+  },
+  appBarSpacer: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    height: "100vh",
+    overflow: "auto"
+  },
+  container: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4)
+  },
+  paper: {
+    padding: theme.spacing(2),
+    display: "flex",
+    overflow: "auto",
+    flexDirection: "column"
+  },
+  fixedHeight: {
+    height: 240
+  }
+}));
+
 const drawerListItems = (
   <div>
-  <ListItem key="departments">
+    <ListItem button>
+      <ListItemIcon>
+        <KitchenIcon />
+      </ListItemIcon>
       <NavLink
         activeClassName="is-active"
         className="navbar-item"
         to="/departments"
       >
-        Отделы
+        <ListItemText primary="Отделы" />
       </NavLink>
     </ListItem>
-    <ListItem key="educations">
+    <ListItem button>
       <NavLink
         activeClassName="is-active"
         className="navbar-item"
         to="/educations"
       >
-        Обучение
+        <ListItemText primary="Обучение" />
       </NavLink>
     </ListItem>
-    <ListItem key="kinds">
-      <NavLink
-        activeClassName="is-active"
-        className="navbar-item"
-        to="/kinds"
-      >
-        Типы
+    <ListItem button>
+      <NavLink activeClassName="is-active" className="navbar-item" to="/kinds">
+        <ListItemText primary="Типы" />
       </NavLink>
     </ListItem>
-    <ListItem key="posts">
-      <NavLink
-        activeClassName="is-active"
-        className="navbar-item"
-        to="/posts"
-      >
-        Должности
+    <ListItem button>
+      <NavLink activeClassName="is-active" className="navbar-item" to="/posts">
+        <ListItemText primary="Должности" />
       </NavLink>
     </ListItem>
-    <ListItem key="practices">
+    <ListItem button>
       <NavLink
         activeClassName="is-active"
         className="navbar-item"
         to="/practices"
       >
-        Учения
+        <ListItemText primary="Учения" />
       </NavLink>
     </ListItem>
-    <ListItem key="ranks">
-      <NavLink
-        activeClassName="is-active"
-        className="navbar-item"
-        to="/ranks"
-      >
-        Чины
+    <ListItem button>
+      <NavLink activeClassName="is-active" className="navbar-item" to="/ranks">
+        <ListItemText primary="Чины" />
       </NavLink>
     </ListItem>
-    <ListItem key="scopes">
-      <NavLink
-        activeClassName="is-active"
-        className="navbar-item"
-         to="/scopes"
-      >
-        Сферы
+    <ListItem button>
+      <NavLink activeClassName="is-active" className="navbar-item" to="/scopes">
+        <ListItemText primary="Сферы" />
       </NavLink>
     </ListItem>
-    <ListItem key="certificates">
+    <ListItem button>
       <NavLink
         activeClassName="is-active"
         className="navbar-item"
         to="/certificates"
       >
-        Удостоверения
+        <ListItemText primary="Удостоверения" />
       </NavLink>
     </ListItem>
     <Divider />
-    <ListItem key="sirentypes">
+    <ListItem button>
       <NavLink
         activeClassName="is-active"
         className="navbar-item"
         to="/sirentypes"
       >
-        Типы сирен
+        <ListItemText primary="Типы сирен" />
       </NavLink>
     </ListItem>
-    </div>
+  </div>
 );
 
 const userItem = (
   <div>
-  <ListItem key="name">
-    {/* <Icon>"user"</Icon> */}
-    name
-  </ListItem>
-  <ListItem key="exit">
-    <Button color="link">Выход</Button>
-  </ListItem>
-</div>
-)
+    <ListItem key="name">
+      {/* <Icon>"user"</Icon> */}
+      name
+    </ListItem>
+    <ListItem key="exit">
+      <Button color="link">Выход</Button>
+    </ListItem>
+  </div>
+);
 
 const topItem = (
   <div>
-  <ListItem key="home">
-
-  <NavLink
-    activeClassName="is-active"
-    className="navbar-item"
-    exact={true}
-    to="/"
-  >
-    <ListItemIcon>
-    ЕДДС
-    </ListItemIcon>
-  </NavLink>
-</ListItem>
-<ListItem key="contacts">
-  <NavLink
-    activeClassName="is-active"
-    className="navbar-item"
-    to="/contacts"
-  >
-    <ListItem type="contacts" />
-    Контакты
-  </NavLink>
-</ListItem>
-<ListItem key="companies">
-  <NavLink
-    activeClassName="is-active"
-    className="navbar-item"
-    to="/companies"
-  >
-    <ListItemIcon type="bank" />
-    Организации
-  </NavLink>
-</ListItem>
-<ListItem key="sirens">
-  <NavLink
-    activeClassName="is-active"
-    className="navbar-item"
-    to="/sirens"
-  >
-    <ListItemIcon type="wifi" />
-    Сирены
-  </NavLink>
-</ListItem>
-</div>
-)
+    <ListItem key="home">
+      <NavLink
+        activeClassName="is-active"
+        className="navbar-item"
+        exact={true}
+        to="/"
+      >
+        <ListItemIcon>ЕДДС</ListItemIcon>
+      </NavLink>
+    </ListItem>
+    <ListItem key="contacts">
+      <NavLink
+        activeClassName="is-active"
+        className="navbar-item"
+        to="/contacts"
+      >
+        <ListItem type="contacts" />
+        Контакты
+      </NavLink>
+    </ListItem>
+    <ListItem key="companies">
+      <NavLink
+        activeClassName="is-active"
+        className="navbar-item"
+        to="/companies"
+      >
+        <ListItemIcon type="bank" />
+        Организации
+      </NavLink>
+    </ListItem>
+    <ListItem key="sirens">
+      <NavLink activeClassName="is-active" className="navbar-item" to="/sirens">
+        <ListItemIcon type="wifi" />
+        Сирены
+      </NavLink>
+    </ListItem>
+  </div>
+);
 
 export default function NavBar() {
+  const classes = useStyles();
   const auth = true;
   const logged = true;
   const [open, setOpen] = React.useState(true);
@@ -195,18 +258,31 @@ export default function NavBar() {
   };
 
   return (
-    <div>
-        <AppBar position="absolute" className={clsx(open)}>
-        <Toolbar>
+    <div className={classes.root}>
+      <AppBar
+        position="absolute"
+        className={clsx(classes.appBar, open && classes.appBarShift)}
+      >
+        <Toolbar className={classes.toolbar}>
           <IconButton
-              edge="start"
-              color="inherit"
-              aria-label="Open drawer"
-              onClick={handleDrawerOpen}
-            >
-              <MenuIcon />
+            edge="start"
+            color="inherit"
+            aria-label="Open drawer"
+            onClick={handleDrawerOpen}
+            className={clsx(
+              classes.menuButton,
+              open && classes.menuButtonHidden
+            )}
+          >
+            <MenuIcon />
           </IconButton>
-          <Typography component="h1" variant="h6" color="inherit" noWrap>
+          <Typography
+            component="h1"
+            variant="h6"
+            color="inherit"
+            noWrap
+            className={classes.title}
+          >
             Dashboard
           </Typography>
           <IconButton color="inherit">
@@ -214,24 +290,24 @@ export default function NavBar() {
               <NotificationsIcon />
             </Badge>
           </IconButton>
-          </Toolbar>
-        </AppBar>
+        </Toolbar>
+      </AppBar>
 
-
-        <Drawer
+      <Drawer
         variant="permanent"
         classes={{
+          paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose)
         }}
         open={open}
       >
-        <div>
-      <IconButton onClick={handleDrawerClose}>
-        <ChevronLeftIcon />
-      </IconButton>
-    </div>
-    <Divider />
-    <List>{drawerListItems}</List>
-  </Drawer>
+        <div className={classes.toolbarIcon}>
+          <IconButton onClick={handleDrawerClose}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </div>
+        <Divider />
+        <List>{drawerListItems}</List>
+      </Drawer>
     </div>
   );
-};
+}
