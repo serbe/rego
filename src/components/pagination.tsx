0570e35937be0f9a4c0397e48fb3c7056845f677
mkoctuk <mkoctuk@gmail.com>
2020-04-21 20:30:55 +0300

    less code

--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react';
 
 interface PaginationProps {
+  centered?: boolean;
   currentPage: number;
   lastPage: number;
   callback: Function;
@@ -8,131 +9,77 @@ interface PaginationProps {
   size?: 'small' | 'normal' | 'medium' | 'large';
 }
 
-interface PreviousProps {
-  currentPage: number;
-  callback: Function;
-}
-
-interface NextProps {
-  currentPage: number;
-  lastPage: number;
-  callback: Function;
-}
-
 interface ItemProps {
-  currentPage: number;
-  callback: Function;
   check: boolean;
   index: number;
   link?: number;
   ellipsis?: boolean;
 }
 
-const Previous = (properties: PreviousProps): JSX.Element | null => {
-  const { currentPage, callback } = properties;
-  return currentPage > 1 ? (
-    <a
-      className="pagination-previous"
-      onClick={(): void => callback(currentPage - 1)}
-      key="PaginationPrev"
-      href="#prev"
-    >
-      Назад
-    </a>
-  ) : null;
-};
-
-const Next = (properties: NextProps): JSX.Element | null => {
-  const { currentPage, lastPage, callback } = properties;
-  return currentPage < lastPage ? (
-    <a
-      className="pagination-next"
-      onClick={(): void => callback(currentPage + 1)}
-      key="PaginationNext"
-      href="#next"
-    >
-      Далее
-    </a>
-  ) : null;
-};
-
-const Item = (properties: ItemProps): JSX.Element | null => {
-  const { currentPage, callback, check, index, link, ellipsis } = properties;
-  const Tag = (): JSX.Element => {
-    return ellipsis ? (
-      <span className="pagination-ellipsis">&hellip;</span>
-    ) : (
-      <a
-        className={link === currentPage ? 'pagination-link is-current' : 'pagination-link'}
-        onClick={link === currentPage ? undefined : (): void => callback(link)}
-        href="#item"
-      >
-        {link}
-      </a>
-    );
-  };
-  return check ? (
-    <li key={`li${index}`}>
-      <Tag />
-    </li>
-  ) : null;
-};
-
 export const Pagination: FC<PaginationProps> = (properties: PaginationProps) => {
-  const { currentPage, lastPage, callback, rounded, size } = properties;
+  const { currentPage, lastPage, callback, centered, rounded, size } = properties;
 
   const sizeClass = size ? `is-${size}` : '';
-  const navClasses = `pagination is-centered ${sizeClass} ${rounded ? 'is-rounded' : ''}`;
+  const navClasses = `pagination ${sizeClass} ${rounded ? 'is-rounded' : ''} ${
+    centered ? 'is-centered' : ''
+  }`;
+
+  const Previous = (): JSX.Element | null =>
+    currentPage > 1 ? (
+      <a
+        className="pagination-previous"
+        onClick={(): void => callback(currentPage - 1)}
+        key="PaginationPrev"
+        href="#prev"
+      >
+        Назад
+      </a>
+    ) : null;
+
+  const Next = (): JSX.Element | null =>
+    currentPage < lastPage ? (
+      <a
+        className="pagination-next"
+        onClick={(): void => callback(currentPage + 1)}
+        key="PaginationNext"
+        href="#next"
+      >
+        Далее
+      </a>
+    ) : null;
+
+  const Item = (properties: ItemProps): JSX.Element | null => {
+    const { check, index, link, ellipsis } = properties;
+
+    return check ? (
+      <li key={`li${index}`}>
+        {ellipsis ? (
+          <span className="pagination-ellipsis">&hellip;</span>
+        ) : (
+          <a
+            className={link === currentPage ? 'pagination-link is-current' : 'pagination-link'}
+            onClick={link === currentPage ? undefined : (): void => callback(link)}
+            href="#item"
+          >
+            {link}
+          </a>
+        )}
+      </li>
+    ) : null;
+  };
 
   return (
-    <nav className={navClasses} key="nav">
-      <Previous currentPage={currentPage} callback={callback} />
-      <Next currentPage={currentPage} lastPage={lastPage} callback={callback} />
+    <nav className={navClasses} key="pagination" role="navigation" aria-label="pagination">
+      <Previous />
+      <Next />
       <ul className="pagination-list" key="ul">
-        <Item
-          currentPage={currentPage}
-          callback={callback}
-          check={currentPage !== 1}
-          index={1}
-          link={1}
-          ellipsis={false}
-        />
-        <Item
-          currentPage={currentPage}
-          callback={callback}
-          check={currentPage > 3}
-          index={2}
-          ellipsis
-        />
-        <Item
-          currentPage={currentPage}
-          callback={callback}
-          check={currentPage > 2}
-          index={3}
-          link={currentPage - 1}
-        />
-        <Item currentPage={currentPage} callback={callback} check index={4} link={currentPage} />
-        <Item
-          currentPage={currentPage}
-          callback={callback}
-          check={currentPage < currentPage - 1}
-          index={5}
-          link={currentPage + 1}
-        />
-        <Item
-          currentPage={currentPage}
-          callback={callback}
-          check={currentPage < lastPage - 2}
-          index={6}
-          ellipsis
-        />
-        <Item
-          currentPage={currentPage}
-          callback={callback}
-          check={currentPage !== lastPage}
-          index={7}
-          link={lastPage}
-        />
+        <Item check={currentPage !== 1} index={1} link={1} />
+        <Item check={currentPage > 3} index={2} ellipsis />
+        <Item check={currentPage > 2} index={3} link={currentPage - 1} />
+        <Item check index={4} link={currentPage} />
+        <Item check={currentPage < lastPage - 1} index={5} link={currentPage + 1} />
+        <Item check={currentPage < lastPage - 2} index={6} ellipsis />
+        <Item check={currentPage !== lastPage} index={7} link={lastPage} />
       </ul>
     </nav>
   );
