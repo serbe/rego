10d46b8fd589b05ca1148aede1e689f014abeb58
Serbe <mkoctuk@gmail.com>
2020-04-22 17:13:43 +0300

    fix centering

--- src/rugo.tsx
+++ src/rugo.tsx
@@ -2,13 +2,13 @@ import React, { FC } from 'react';
 import { Router } from './containers/Router';
 
 import { NavBar } from './components/navbar';
-// import './rugo.scss';
+import './rugo.css';
 
 const Rugo: FC = () => {
   return (
     <>
       <NavBar />
-      <div className="container">
+      <div className="container pv1">
         <Router />
       </div>
     </>
