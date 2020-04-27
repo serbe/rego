ae69c4f19f0bed134ea8eaabf443b095296ef7f9
Serbe <mkoctuk@gmail.com>
2020-04-27 15:58:04 +0300

    select like vugo

--- src/components/select.tsx
+++ src/components/select.tsx
@@ -1,66 +1,77 @@
-import React, { useState, SyntheticEvent, useEffect, FC } from 'react';
+import React, { useState, useEffect, FC } from 'react';
 
 import { Icon } from './icon';
 import { SelectItem } from '../models/selectitem';
+import { rws } from '../netapi';
 
 import './select.css';
 
 interface SelectProps {
   id?: number;
-  iconLeft?: string;
-  iconRight?: string;
+  icon?: string;
+  // iconRight?: string;
   color?: 'primary' | 'info' | 'success' | 'warning' | 'danger';
-  size?: 'small' | 'normal' | 'medium' | 'large';
-  state?: string;
+  // state?: string;
   label?: string;
-  list: SelectItem[];
-  itemName?: string;
-  inputReference?: {
-    <T>(initialValue: T): React.MutableRefObject<T>;
-    <T>(initialValue: T | null): React.RefObject<T>;
-    <T = undefined>(): React.MutableRefObject<T | undefined>;
-  };
+  listName?: string;
+  callback: (event: number) => void;
 }
 
-export const Select: FC<SelectProps> = (properties: SelectProps) => {
-  const {
-    id,
-    iconLeft,
-    iconRight,
-    color,
-    size,
-    state,
-    label,
-    list,
-    itemName,
-    inputReference,
-  } = properties;
+type CLWS = {
+  name: string;
+  object: {
+    SelectItem?: SelectItem[];
+  };
+  error?: string;
+};
 
-  if (list[0].id !== 0) {
-    list.unshift({ id: 0, name: '' });
-  }
+export const Select: FC<SelectProps> = (properties: SelectProps) => {
+  const { id, label, icon, color, listName, callback } = properties;
+  // const { id, iconLeft, iconRight, color, state, label, listName, callback } = properties;
 
   const [opened, setOpened] = useState(false);
   const [itemID, setItemID] = useState(id ? id : 0);
-  const [filteredList, setFilteredList] = useState(list);
+  const [list, setList] = useState<SelectItem[]>([{ id: 0, name: '' }]);
+  const [error, setError] = useState<string>();
+  const [value, setValue] = useState<string>();
 
-  // useEffect(() => {
-  //   const fList =
-  //     item.name.length > 0
-  //       ? list.filter(
-  //           (listItem) => listItem.name === '' || new RegExp(item.name, 'i').exec(listItem.name),
-  //         )
-  //       : list;
-  //   setFilteredList(fList);
-  // }, [item.name, list]);
+  useEffect(() => {
+    rws.addEventListener('message', (message: MessageEvent) => {
+      const data: CLWS = JSON.parse(message.data);
+      if (data?.name === listName && data.object.SelectItem) {
+        setList(data.object.SelectItem);
+      }
 
-  const controlClasses = `control is-expanded select is-fullwidth ${
-    iconLeft ? 'has-icons-left' : ''
-  } ${iconRight ? 'has-icons-right' : ''}`;
+      if (data.error) {
+        setError(data.error);
+      }
+    });
+    rws.send(`{"Get":{"List":"${listName}"}}`);
 
-  const inputClasses = `input ${color ? `is-${color}` : ''} ${size ? `is-${size}` : ''} ${
-    state ? `is-${state}` : ''
-  }`;
+    return function cleanup(): void {
+      rws.removeEventListener('message', (message: unknown) => {
+        console.log('removeEventListener', message);
+      });
+    };
+  }, [listName]);
+
+  useEffect(() => {
+    if (list[0].id !== 0) {
+      list.unshift({ id: 0, name: '' });
+    }
+    if (!id && id === 0) {
+      setValue('');
+    } else {
+      const currentItem = list.find((item) => item.id === id);
+      setValue(currentItem ? currentItem.name : '');
+    }
+  }, [list, id]);
+
+  // const controlClasses = `control is-expanded select is-fullwidth ${
+  //   iconLeft ? 'has-icons-left' : ''
+  // } ${iconRight ? 'has-icons-right' : ''}`;
+
+  // const inputClasses = `input ${color ? `is-${color}` : ''} ${state ? `is-${state}` : ''}`;
 
   const Label = (): JSX.Element | null =>
     label ? (
@@ -69,72 +80,85 @@ export const Select: FC<SelectProps> = (properties: SelectProps) => {
       </label>
     ) : null;
 
-  const IconLeft = (): JSX.Element | null =>
-    iconLeft ? (
+  const LeftIcon = (): JSX.Element | null =>
+    icon ? (
       <Icon
-        icon={iconLeft}
+        icon={icon}
         position="left"
         color={color !== 'primary' ? color : undefined}
-        size={size}
         key="SelectIconLeft"
       />
     ) : null;
 
-  const IconRight = (): JSX.Element | null =>
-    iconRight ? (
-      <Icon
-        icon={iconRight}
-        position="right"
-        color={color !== 'primary' ? color : undefined}
-        size={size}
-        key="SelectIconRight"
-      />
-    ) : null;
-
-  const DropdownContent = (): JSX.Element => (
-    <div className="dropdown-content ddm">
-      {list.map((ListItem) => (
-        <a
-          className="dropdown-item"
-          key={ListItem.id}
-          href={`#${ListItem.name}`}
-          onClick={(): void => {
-            setItemID(ListItem.id);
-          }}
-        >
-          {ListItem.name}
-        </a>
-      ))}
-    </div>
-  );
+  // const IconRight = (): JSX.Element | null =>
+  //   iconRight ? (
+  //     <Icon
+  //       icon={iconRight}
+  //       position="right"
+  //       color={color !== 'primary' ? color : undefined}
+  //       key="SelectIconRight"
+  //     />
+  //   ) : null;
 
   const currentValue = (): string => {
-    const value = list.find((item) => item.id === itemID);
-    return value ? value.name : '';
+    if (opened) {
+      return value ? value : '';
+    }
+    const currentItem = list.find((item) => item.id === itemID);
+    return currentItem ? currentItem.name : '';
   };
+
+  const filteredList = (): SelectItem[] => {
+    const inputValue = currentValue();
+    return inputValue.length > 0
+      ? list.filter(
+          (listItem) => listItem.name === '' || new RegExp(inputValue, 'i').exec(listItem.name),
+        )
+      : list;
+  };
+
+  const DropdownContent = (): JSX.Element | null =>
+    error || !opened ? null : (
+      <div className="select-box">
+        {filteredList().map((ListItem) => (
+          <div
+            className="select-item"
+            key={ListItem.id}
+            onClick={(): void => {
+              setItemID(ListItem.id);
+              setValue(ListItem.name);
+              callback(ListItem.id);
+            }}
+          >
+            {ListItem.name}
+          </div>
+        ))}
+      </div>
+    );
 
   return (
     <div className="field">
-      <div className={`dropdown full-width ${opened ? 'is-active' : ''}`}>
-        <div className="dropdown-trigger full-width">
-          <input
-            className="input"
-            type="text"
-            aria-haspopup="true"
-            aria-controls="dropdown-menu"
-            defaultValue={currentValue()}
-            onFocus={(): void => {
-              setOpened(true);
-            }}
-            onBlur={(): void => {
-              setTimeout(() => setOpened(false), 300);
-            }}
-          />
-        </div>
-        <div className="dropdown-menu full-width" id="dropdown-menu" role="menu">
-          <DropdownContent />
-        </div>
+      <Label />
+      <div className={`control is-expanded select is-fullwidth ${icon ? 'has-icons-left' : ''}`}>
+        <input
+          className={`input ${color ? `is-${color}` : ''}`}
+          type="text"
+          aria-haspopup="true"
+          aria-controls="dropdown-menu"
+          value={currentValue()}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>): void => {
+            setValue(event.target.value);
+          }}
+          onFocus={(): void => {
+            setOpened(true);
+          }}
+          onBlur={(): void => {
+            setTimeout(() => setOpened(false), 300);
+          }}
+        />
+        <LeftIcon />
       </div>
+      <DropdownContent />
     </div>
   );
 };
