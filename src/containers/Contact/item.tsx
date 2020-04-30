d3346cbf01b9b6d4e288ebb5a8d436d6c973f940
Serbe <mkoctuk@gmail.com>
2020-04-30 13:33:49 +0300

    try to add input groups

--- src/containers/Contact/item.tsx
+++ src/containers/Contact/item.tsx
@@ -5,8 +5,10 @@ import { Contact } from '../../models/contact';
 import { SelectItem } from '../../models/selectitem';
 import { rws } from '../../netapi';
 import { FormField } from '../../components/formfield';
+import { Input } from '../../components/input';
 import { Select } from '../../components/select';
 import { DatePicker } from '../../components/datepicker';
+import { addEmptyString, numberToString } from '../../helpers/utils';
 
 type CLWS = {
   name: string;
@@ -19,6 +21,67 @@ type CLWS = {
 
 const ContactForm = (contact: Contact): JSX.Element => {
   const c = contact;
+
+  let emails = addEmptyString(c.emails);
+  let phones = addEmptyString(numberToString(c.phones));
+  let faxes = addEmptyString(numberToString(c.faxes));
+
+  const Emails = (): JSX.Element => (
+    <div className="field">
+      <label className="label">Электронный адрес</label>
+      {emails.map((email, index) => (
+        <Input
+          icon="envelope"
+          key={`email-${index}`}
+          defaultValue={email}
+          placeholder="Электронный адрес"
+          onBlur={(event): void => {
+            emails[index] = event.target.value;
+            emails = addEmptyString(emails);
+          }}
+        />
+      ))}
+    </div>
+  );
+
+  const Phones = (): JSX.Element => (
+    <div className="field">
+      <label className="label">Телефон</label>
+      {phones.map((phone, index) => (
+        <Input
+          type="tel"
+          icon="phone"
+          key={`phone-${index}`}
+          defaultValue={phone.toString()}
+          placeholder="Телефон"
+          onBlur={(event): void => {
+            phones[index] = event.target.value;
+            phones = addEmptyString(phones);
+            console.log(event.target.value, index, phones);
+          }}
+        />
+      ))}
+    </div>
+  );
+
+  const Faxes = (): JSX.Element => (
+    <div className="field">
+      <label className="label">Факс</label>
+      {faxes.map((fax, index) => (
+        <Input
+          type="tel"
+          icon="fax"
+          key={`fax-${index}`}
+          defaultValue={fax.toString()}
+          placeholder="Факс"
+          onBlur={(event): void => {
+            faxes[index] = event.target.value;
+            faxes = addEmptyString(faxes);
+          }}
+        />
+      ))}
+    </div>
+  );
 
   return (
     <div>
@@ -99,6 +162,18 @@ const ContactForm = (contact: Contact): JSX.Element => {
               c.birthday = value;
             }}
           />
+        </div>
+      </div>
+
+      <div className="columns">
+        <div className="column">
+          <Emails />
+        </div>
+        <div className="column">
+          <Phones />
+        </div>
+        <div className="column">
+          <Faxes />
         </div>
       </div>
     </div>
